0ad9af34eced4d64312623bc0a1793577d5524cc
Alex Coleman <LordMailman@users.noreply.github.com>
2018-07-01 10:24:40 -0400

    Added mean/max/min methods to the Die class

--- dice.py
+++ dice.py
@@ -25,9 +25,9 @@ class Die(object):
 
         # Calculate properties
         len_values = len(values)
-        self.max_value = max(values)
-        self.min_value = min(values)
-        self.mean_value = (self.max_value + self.min_value)/2
+        self._max_value = max(values)
+        self._min_value = min(values)
+        self._mean_value = (self._max_value + self._min_value)/2
         # self.mean_value = sum(self.values)/len_values #TODO: See if numpy.mean is faster
         self.probability = 1.0/len_values
 
@@ -40,9 +40,8 @@ class Die(object):
 
         # TODO:All dice are the same, add UIDs to track individual dice
 
-    def roll(self, num_rolls):
+    def roll(self, num_rolls=1):
         """Roll N dice and return a list of values."""
-
         # Check Die
         if not self.values:
             raise ValueError('Values must not be empty in order to Die.roll().')
@@ -50,6 +49,18 @@ class Die(object):
         # Roll <num_rolls> times
         rolls = [randint(0, len(self.values)-1) for i in range(num_rolls)]
         return [self.values[i] for i in rolls]
+
+    def min(self, num_rolls=1):
+        """Minimum value of N dice rolls."""
+        return num_rolls * self._min_value
+
+    def max(self, num_rolls=1):
+        """Maximum value of N dice rolls."""
+        return num_rolls * self._max_value
+
+    def mean(self, num_rolls=1):
+        """Mean value of N dice rolls."""
+        return num_rolls * self._mean_value
 
 
 # Self-test
@@ -78,17 +89,17 @@ if __name__ == '__main__':
 
     # Real input for 1d0
     TEST_D0 = Die(0)
-    assert TEST_D0.max_value == 0
-    assert TEST_D0.max_value == 0
-    assert isclose(0.0, TEST_D0.mean_value)
+    assert TEST_D0.max() == 0
+    assert TEST_D0.max() == 0
+    assert isclose(0.0, TEST_D0.mean())
     assert isclose(1.0, TEST_D0.probability)
 
     # Real input for 1d20
     TEST_D20 = Die(20)
-    assert TEST_D20.max_value == 20
-    assert TEST_D20.min_value == 1
-    assert isclose(10.5, TEST_D20.mean_value)
-    assert isclose(1.0/TEST_D20.max_value, TEST_D20.probability)
+    assert TEST_D20.max() == 20
+    assert TEST_D20.min() == 1
+    assert isclose(10.5, TEST_D20.mean())
+    assert isclose(1.0/TEST_D20.max(), TEST_D20.probability)
 
     print('PASSED')
 
@@ -96,11 +107,22 @@ if __name__ == '__main__':
 
     # Test die roll
     TEST_ROLL = TEST_D20.roll()
-    assert isinstance(TEST_ROLL, int)
-    assert TEST_ROLL >= TEST_D20.min_value
-    assert TEST_ROLL <= TEST_D20.max_value
-    assert TEST_ROLL in TEST_D20.values
-    print(TEST_ROLL, end='...')
+    assert isinstance(TEST_ROLL, list)
+    assert TEST_ROLL[0] >= TEST_D20.min()
+    assert TEST_ROLL[0] <= TEST_D20.max()
+    assert TEST_ROLL[0] in TEST_D20.values
+    print(TEST_ROLL[0], end='...')
+
+    # Test no roll
+    TEST_ROLL = TEST_D20.roll(0)
+    assert isinstance(TEST_ROLL, list)
+    assert not TEST_ROLL
+
+    # Test multiple rolls
+    TEST_ROLL = TEST_D20.roll(3)
+    assert isinstance(TEST_ROLL, list)
+    for i in enumerate(TEST_ROLL):
+        print(TEST_ROLL[i[0]], end='...')
 
     print('PASSED')
 
